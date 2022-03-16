4737565071a696f7dbf0db9cdb3c3a06faee3ad8
Nithyadevi <nithi2218@gmail.com>
2022-03-16 12:21:40 +1100

    5 Errors

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);    
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(2, 4);
+}
+
+
+function createFareMultiplier(num) {
+    return function fareQuintupler() {
+        return num*num;
+    }
+}createFareMultiplier(5);
+
+
+function fareDoubler(num) {
+    return num * 2;
+}
+
+const fareTripler = (num) => num * 3;
+
+function selectingDrivers(drivers) {
+    drivers[0]= returnFirstTwoDrivers;
+    return drivers[0];
+}
+
+
+const selectDifferentDrivers = (arrayOfDrivers, function1) => {
+    return function1;
+}
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+selectDifferentDrivers(drivers, returnLastTwoDrivers);
+
